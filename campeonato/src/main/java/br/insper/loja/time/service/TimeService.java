a0c4502c48c70f228c76f60b803f2bc428679a4b
Henrique Mayor <henriquessm@al.insper.edu.br>
2024-09-30 20:46:45 -0300

    a

---
+++ campeonato/src/main/java/br/insper/loja/time/service/TimeService.java
@@ -0,0 +1,46 @@
+package br.insper.loja.time.service;
+
+import br.insper.loja.partida.dto.RetornarPartidaDTO;
+import br.insper.loja.time.exception.TimeNaoEncontradoException;
+import br.insper.loja.time.repository.TimeRepository;
+import br.insper.loja.time.model.Time;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TimeService {
+
+
+
+    @Autowired
+    private TimeRepository timeRepository;
+
+    public Time cadastrarTime(Time time) {
+        if (time.getNome().equals("")
+                || time.getIdentificador().equals("")) {
+            throw  new RuntimeException("Dados invalidos");
+        } else {
+            return timeRepository.save(time);
+        }
+    }
+
+    public List<Time> listarTimes(String estado) {
+        if (estado != null) {
+            return timeRepository.findByEstado(estado);
+        }
+        return timeRepository.findAll();
+    }
+
+    public Time getTime(Integer id) {
+        Optional<Time> op = timeRepository.findById(id);
+        if (op.isPresent()) {
+            return op.get();
+        }
+        throw new TimeNaoEncontradoException("Time " + id + " não encontrado");
+    }
+
+}
